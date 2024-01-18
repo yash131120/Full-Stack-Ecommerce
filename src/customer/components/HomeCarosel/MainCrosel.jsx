18fe8e2664fcc554e19b623b1eb3a5009c34914b
yash vavaliya <139432375+yash131120@users.noreply.github.com>
2024-01-18 14:06:45 +0530

    Update MainCrosel.jsx

--- src/customer/components/HomeCarosel/MainCrosel.jsx
+++ src/customer/components/HomeCarosel/MainCrosel.jsx
@@ -25,14 +25,6 @@ export default MainCrosel
 
 
 
-// import React from 'react';
-// import AliceCarousel from 'react-alice-carousel';
-// import 'react-alice-carousel/lib/alice-carousel.css';
-// import { mainCarouselData } from './MainCaroselData';
-
-
-
-
 // const MainCrosel = () => (
 //   // const navigate=useNavi
 //   // const items = mainCarouselData.map((item)=> <img className='cursor-pointer' role='presentation' src={item.image} alt=''/>)
