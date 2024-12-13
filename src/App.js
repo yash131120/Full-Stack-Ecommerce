a165464bcd7d568fe0e5165f39105a69f0162589
yash vavaliya <139432375+yash131120@users.noreply.github.com>
2024-12-13 09:00:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
   return (
     <div className="">
       <Routes>
+    // customerRoutrs link page
         <Route path="/*" element={<CustomerRouters />}></Route>
       </Routes>
     </div>
